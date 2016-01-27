36cb016dda6d6b32fc54a357b70bd2420c1bb900
Metsune <stevensikes@me.com>
2016-01-26 19:21:23 -0600

    Test

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	
+	private static MusicPlayer instance = null;
+	
+	public static MusicPlayer Instance {
+		get { return instance; }
+	}
+	
+	void Awake() {
+		if(instance != null && instance != this){
+			Destroy(this.gameObject);
+			Debug.Log("Self destructing");
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}	
+}
+
